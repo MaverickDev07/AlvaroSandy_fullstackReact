efc4b47e5bad25200cf4c23460c09fbfb81496cb
MaverickDev07 <alvarexdb07@gmail.com>
2025-09-07 17:37:59 -0400

    build listo para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/AlvaroSandy_fullstackReact/',
+  build: {
+    outDir: 'docs', // importante para usar main/docs en GitHub Pages
+  },
 })
